96ab802dfd506d3229e7ae3fc0779a8e3910990f
nikhitha126 <mandlanikhitha@gmail.com>
2024-12-14 14:52:10 +0530

    Update Grade_streamlit.py

--- Grade_streamlit.py
+++ Grade_streamlit.py
@@ -4,9 +4,9 @@ project= st.number_input("Enter project score")
 internal= st.number_input("internal score: ")
 external=st.number_input("external score: ")
 if st.button("Grade"):
-if(project>=50 and internal >=50 and external >= 50):
- total= project*(70/100)+ internal*(10/100) + external*(20/100)
- st.write(f'total:{total}')
+ if(project>=50 and internal >=50 and external >= 50):
+  total= project*(70/100)+ internal*(10/100) + external*(20/100)
+  st.write(f'total:{total}')
  if(total > 90):
   st.write("A grade")
  elif(total>80 and total<=90):
